1abbcf5f771080baf32611d66894c949b73d2384
labra <jelabra@gmail.com>
2016-03-26 23:32:24 +0100

    Updated stats.sh

--- stats/test.sh
+++ stats/test.sh
@@ -1,89 +1,66 @@
 values="1 2" 
 # values="1 5 10 50 100 500 1000" 
-wiGenRunner="\basura\wigen-0.0.1\wigen-0.0.1\bin\wigen.bat"
-# wiGenRunner=./wiGen
+#wiGenRunner="\basura\wigen-0.0.1\wigen-0.0.1\bin\wigen.bat"
+wiGenRunner=./wiGen
 
-countries="1"
-dataSets="1"
-slices="1"
-obs="1"
-comps="1"
-indicators="1"
-orgs="1"
-
-for countries in $values
+for value in $values
 do
 outName="outCountries"
-params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
+params="-c $value -d 1 --slices 1 -o 1 -p 1 -i 1 -g 1 --shex --time"
 paramsSingle="$params --no-scopeNodes"
 paramsBad="$params --badCountries 1"
 $wiGenRunner $params >> $outName 
 $wiGenRunner $paramsSingle >> $outName
 $wiGenRunner $paramsBad >> $outName
-done
 
-for dataSets in $values
-do
 outName="outDataSets"
-params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
+params="-c 1 -d $value --slices 1 -o 1 -p 1 -i 1 -g 1 --shex --time"
 paramsSingle="$params --no-scopeNodes"
 paramsBad="$params --badCountries 1"
 $wiGenRunner $params >> $outName 
 $wiGenRunner $paramsSingle >> $outName
 $wiGenRunner $paramsBad >> $outName
+
+outName="outSlices"
+params="-c 1 -d 1 --slices $value -o 1 -p 1 -i 1 -g 1 --shex --time"
+paramsSingle="$params --no-scopeNodes"
+paramsBad="$params --badCountries 1"
+$wiGenRunner $params >> $outName 
+$wiGenRunner $paramsSingle >> $outName
+$wiGenRunner $paramsBad >> $outName
+
+outName="outObs"
+params="-c 1 -d 1 --slices 1 -o $value -p 1 -i 1 -g 1 --shex --time"
+paramsSingle="$params --no-scopeNodes"
+paramsBad="$params --badCountries 1"
+$wiGenRunner $params >> $outName 
+$wiGenRunner $paramsSingle >> $outName
+$wiGenRunner $paramsBad >> $outName
+
+outName="outComps"
+params="-c 1 -d 1 --slices 1 -o 1 -p $value -i 1 -g 1 --shex --time"
+paramsSingle="$params --no-scopeNodes"
+paramsBad="$params --badCountries 1"
+$wiGenRunner $params >> $outName 
+$wiGenRunner $paramsSingle >> $outName
+$wiGenRunner $paramsBad >> $outName
+
+outName="outIndicators"
+params="-c 1 -d 1 --slices 1 -o 1 -p 1 -i $value -g 1 --shex --time"
+paramsSingle="$params --no-scopeNodes"
+paramsBad="$params --badCountries 1"
+$wiGenRunner $params >> $outName 
+$wiGenRunner $paramsSingle >> $outName
+$wiGenRunner $paramsBad >> $outName
+
+outName="outOrgs"
+params="-c 1 -d 1 --slices 1 -o 1 -p 1 -i 1 -g $value --shex --time"
+paramsSingle="$params --no-scopeNodes"
+paramsBad="$params --badCountries 1"
+$wiGenRunner $params >> $outName 
+$wiGenRunner $paramsSingle >> $outName
+$wiGenRunner $paramsBad >> $outName
+
 done
 
-for slices in $values
-do
- outName="outSlices"
- params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
- paramsSingle="$params --no-scopeNodes"
- paramsBad="$params --badCountries 1"
- $wiGenRunner $params >> $outName 
- $wiGenRunner $paramsSingle >> $outName
- $wiGenRunner $paramsBad >> $outName
-done
 
-for obs in $values
-do
- outName="outObs"
- params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
- paramsSingle="$params --no-scopeNodes"
- paramsBad="$params --badCountries 1"
- $wiGenRunner $params >> $outName 
- $wiGenRunner $paramsSingle >> $outName
- $wiGenRunner $paramsBad >> $outName
-done
-
-for comps in $values
-do
- outName="outComps"
- params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
- paramsSingle="$params --no-scopeNodes"
- paramsBad="$params --badCountries 1"
- $wiGenRunner $params >> $outName 
- $wiGenRunner $paramsSingle >> $outName
- $wiGenRunner $paramsBad >> $outName
-done
-
-for indicators in $values
-do
- outName="outIndicators"
- params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
- paramsSingle="$params --no-scopeNodes"
- paramsBad="$params --badCountries 1"
- $wiGenRunner $params >> $outName 
- $wiGenRunner $paramsSingle >> $outName
- $wiGenRunner $paramsBad >> $outName
-done
-
-for orgs in $values
-do
- outName="outOrgs"
- params="-c $countries -d $dataSets --slices $slices -o $obs -p $comps -i $indicators -g $orgs --shex --time"
- paramsSingle="$params --no-scopeNodes"
- paramsBad="$params --badCountries 1"
- $wiGenRunner $params >> $outName 
- $wiGenRunner $paramsSingle >> $outName
- $wiGenRunner $paramsBad >> $outName
-done
